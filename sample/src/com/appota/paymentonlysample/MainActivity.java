f3a55feacdde0dcd37a54f186b150c7e8d2b9c6f
Nguyễn Khánh Duy <duynk@appota.com>
2013-11-29 10:38:19 +0700

    add sdk version

--- sample/src/com/appota/paymentonlysample/MainActivity.java
+++ sample/src/com/appota/paymentonlysample/MainActivity.java
@@ -40,65 +40,48 @@ public class MainActivity extends Activity {
 		
 		receiver = new MyReceiver();
 		IntentFilter filter = new IntentFilter();
-		filter.addAction(AppotaAction.BANK_PAYMENT_SUCCESS_ACTION);
-		filter.addAction(AppotaAction.SMS_PAYMENT_SUCCESS_ACTION);
-		filter.addAction(AppotaAction.CARD_PAYMENT_SUCCESS_ACTION);
-		filter.addAction(AppotaAction.PAYPAL_PAYMENT_SUCCESS_ACTION);
-        filter.addAction(AppotaAction.GOOGLE_PAYMENT_SUCCESS_ACTION);
+		filter.addAction(AppotaAction.PAYMENT_SUCCESS_ACTION);
 		registerReceiver(receiver, filter);
 	}
 	
 	public void buySmsOneAmount(View v){
         ap.setContext(this, apiKey, sandboxApiKey);
-		ap.makeSMSPayment(500, "", "duydkny", "http://abc.com", "Nap tien vao de em lay doanh thu", "5000 -> 500 coins \n10000 -> 1000 coins \n15000 -> 1500 coins", new TransactionStatusCallback() {
-			
-			@Override
-			public void onTransactionSuccess(TransactionResult result) {
-				// TODO Auto-generated method stub
-				am.showPositiveDialog("Success", result.toString(), null);
-			}
-			
-			@Override
-			public void onTransactionError(String message) {
-				// TODO Auto-generated method stub
-				am.showPositiveDialog("Error", message, null);
-			}
-		});
+		ap.makeSMSPayment(500, "", "duydkny", "http://abc.com", "Custom title", "5000 -> 500 coins \n10000 -> 1000 coins \n15000 -> 1500 coins");
 	}
 	
 	public void buySmsListAmount(View v) throws AppotaPaymentException {
 		int[] listAmount = new int[]{500, 1000, 3};
-		ap.makeSMSPayment(listAmount, "", "duydkny", "http://abc.com", "Nap tien vao de em lay doanh thu", "5000 -> 500 coins \n10000 -> 1000 coins \n15000 -> 1500 coins");
+		ap.makeSMSPayment(listAmount, "", "duydkny", "http://abc.com", "Custom title", "5000 -> 500 coins \n10000 -> 1000 coins \n15000 -> 1500 coins");
 	}
 	
 	public void buyCard(View v){
-		ap.makeCardPayment("Nap tien vao game", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins", "http://abc.com", "state", "duydkny");
+		ap.makeCardPayment("Custom title", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins", "http://abc.com", "state", "duydkny");
 	}
 	
 	public void buyBankOneAmount(View v){
-		ap.makeBankPayment(20000, "http://abc.com", "", "duydkny", "Nap tien vao game", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
+		ap.makeBankPayment(20000, "http://abc.com", "", "duydkny", "Custom title", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
 	}
 	
 	public void buyBankNoAmount(View v){
-		ap.makeBankPayment("http://abc.com", "", "duydkny", "Nap tien vao game", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
+		ap.makeBankPayment("http://abc.com", "", "duydkny", "Custom title", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
 	}
 	
 	public void buyBankListAmount(View v){
 		int[] amounts = new int[] {20000, 21000, 22000};
-		ap.makeBankPayment(amounts, "http://abc.com", "", "duydkny", "Nap tien vao game", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
+		ap.makeBankPayment(amounts, "http://abc.com", "", "duydkny", "Custom title", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
 	}
 	
 	public void buyPaypalOneAmount(View v){
-		ap.makePaypalPayment(1.00, "http://abc.com", "", "duydkny", "Nap tien vao game", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
+		ap.makePaypalPayment(1.00, "http://abc.com", "", "duydkny", "Custom title", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
 	}
 	
 	public void buyPaypalNoAmount(View v){
-		ap.makePaypalPayment("http://abc.com", "", "duydkny", "Nap tien vao game", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
+		ap.makePaypalPayment("http://abc.com", "", "duydkny", "Custom title", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
 	}
 	
 	public void buyPaypalListAmount(View v){
 		double[] amounts = new double[] {1.00, 2.99, 5.99};
-		ap.makePaypalPayment(amounts, "http://abc.com", "", "duydkny", "Nap tien vao game", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
+		ap.makePaypalPayment(amounts, "http://abc.com", "", "duydkny", "Custom title", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
 	}
 
     public void buyGoogleListAmount(View v){
@@ -110,46 +93,20 @@ public class MainActivity extends Activity {
         listItem.add(new InAppPurchaseItem("appotapaymentsdk_test15", "Test 15"));
         listItem.add(new InAppPurchaseItem("appotapaymentsdk_test16", "Test 16"));
         listItem.add(new InAppPurchaseItem("appotapaymentsdk_test17", "Test 17"));
-        ap.makeGooglePlayPayment(listItem, "http://abc.com", "", "duydkny", "Nap tien vao game", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
+        ap.makeGooglePlayPayment(listItem, "http://abc.com", "", "duydkny", "Custom title", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
     }
 
     public void buyGoogleOneAmount(View v){
         InAppPurchaseItem item = new InAppPurchaseItem("appotapaymentsdk_test12", "Test 12");
-        ap.makeGooglePlayPayment(item, "http://abc.com", "", "duydkny", "Nap tien vao game", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
+        ap.makeGooglePlayPayment(item, "http://abc.com", "", "duydkny", "Custom title", "10000 -> 500 coins \n20000 -> 1000 coins \n50000 -> 1500 coins");
     }
 
 	public class MyReceiver extends AppotaPaymentReceiver {
 
-		@Override
-		public void onBankPaymentSuccess(TransactionResult result) {
-			// TODO Auto-generated method stub
-			//do whatever you want
-			Toast.makeText(MainActivity.this, result.toString(), Toast.LENGTH_SHORT).show();
-		}
-
-		@Override
-		public void onSMSPaymentSuccess(TransactionResult result) {
-			// TODO Auto-generated method stub
-			Toast.makeText(MainActivity.this, result.toString(), Toast.LENGTH_SHORT).show();
-		}
-
-		@Override
-		public void onCardPaymentSuccess(CardPaymentResult result) {
-			// TODO Auto-generated method stub
-			Toast.makeText(MainActivity.this, result.toString(), Toast.LENGTH_SHORT).show();
-		}
-
-		@Override
-		public void onPaypalPaymentSuccess(TransactionResult result) {
-			// TODO Auto-generated method stub
-			Toast.makeText(MainActivity.this, result.toString(), Toast.LENGTH_SHORT).show();
-		}
-
         @Override
-        public void onGooglePaymentSuccess(TransactionResult result) {
-            Toast.makeText(MainActivity.this, result.toString(), Toast.LENGTH_SHORT).show();
+        public void onPaymentSuccess(TransactionResult result) {
+            Toast.makeText(MainActivity.this, "Just for testing: " + result.getTransactionId() + " " + result.getType(), Toast.LENGTH_SHORT).show();
         }
-
     }
 	
 	@Override
